fbb43076ab7f657d4f09bbb2a1f6962aa9d0483e
TypJakob <jakob.hansson314@gmail.com>
2025-11-23 21:39:38 +0100

    added suggestions for future extensions

--- src/main/java/com/grouptwelve/roguelikegame/model/EventsPackage/GameEventListener.java
+++ src/main/java/com/grouptwelve/roguelikegame/model/EventsPackage/GameEventListener.java
@@ -2,17 +2,15 @@ package com.grouptwelve.roguelikegame.model.EventsPackage;
 
 /**
  * Interface for objects that want to observe game events.
- * Different systems (Game logic, Audio, Animation, Particles, etc.)
- * can implement this to react to player actions.
- *
- * This follows the Observer pattern and enables loose coupling
- * between the input system and game systems.
+ * Different systems (Game logic, Audio, Animation, etc.) can implement this to react to player actions.
  */
 public interface GameEventListener {
     void onMovement(MovementEvent event);
-
     void onAttack(AttackEvent event);
 
     // TODO: Extend with more events
-    // onPlayerLevelUp(LevelUpEvent event); - show upgrade screen
+    // onEnemyDeath(EnemyDeathEvent event);
+    // onPlayerDeath(PlayerDeathEvent event);
+    // onPlayerDamaged(PlayerDamagedEvent event);
+    // onLevelUp(LevelUpEvent event);
 }
