5b0549540d895cb82a0dfe29a61721924b77494e
TypJakob <jakob.hansson314@gmail.com>
2025-11-27 14:34:16 +0100

    add KnockbackEffect to weapons

--- src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Club.java
+++ src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Club.java
@@ -1,9 +1,12 @@
 package com.grouptwelve.roguelikegame.model.Weapons;
 
+import com.grouptwelve.roguelikegame.model.EffectsPackage.KnockbackEffect;
+
 public class Club extends Weapon {
 
     public Club() {
         super(20, 40); // damage, range
+        effects.add(new KnockbackEffect(4000));
     }
 /*
     @Override
--- src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Sword.java
+++ src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Sword.java
@@ -1,20 +1,11 @@
 package com.grouptwelve.roguelikegame.model.Weapons;
 
+import com.grouptwelve.roguelikegame.model.EffectsPackage.KnockbackEffect;
+
 public class Sword extends Weapon {
 
     public Sword() {
         super(10, 30); // damage, range
+        effects.add(new KnockbackEffect(2500));
     }
-/*
-    @Override
-    public void attack(Entity attacker, Entity target) {
-        target.takeDamage(damage);
-
-        // Apply effects
-        for (EffectInterface e : effects) {
-            e.apply(target);
-        }
-    }
-
- */
 }
