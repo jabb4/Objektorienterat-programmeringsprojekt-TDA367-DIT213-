abaaada31919406cfbaacfe22d39527806290864
TypJakob <jakob.hansson314@gmail.com>
2025-11-27 15:04:46 +0100

    refactor knockback strength attribute for clarity

--- src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Sword.java
+++ src/main/java/com/grouptwelve/roguelikegame/model/Weapons/Sword.java
@@ -3,9 +3,10 @@ package com.grouptwelve.roguelikegame.model.Weapons;
 import com.grouptwelve.roguelikegame.model.EffectsPackage.KnockbackEffect;
 
 public class Sword extends Weapon {
+    private static final int SWORD_KNOCKBACK_STRENGTH = 2500; // Pixels per second
 
     public Sword() {
         super(10, 30); // damage, range
-        effects.add(new KnockbackEffect(2500));
+        effects.add(new KnockbackEffect(SWORD_KNOCKBACK_STRENGTH));
     }
 }
