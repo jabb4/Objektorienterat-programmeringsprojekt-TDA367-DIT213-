3e2d3b37dcf1d738882cd1fc75fcb7000e4253a0
TypJakob <87580940+TypJakob@users.noreply.github.com>
2025-12-19 16:21:18 +0100

    cleanup unused code

--- src/main/java/com/grouptwelve/roguelikegame/controller/Command.java
+++ src/main/java/com/grouptwelve/roguelikegame/controller/Command.java
@@ -1,7 +1,7 @@
 package com.grouptwelve.roguelikegame.controller;
 
 public enum Command {
-    // Movement // selecting buff
+    // Movement / Selecting buff
     MOVE_UP,
     MOVE_DOWN,
     MOVE_LEFT,
@@ -10,20 +10,9 @@ public enum Command {
     // Actions
     ATTACK,
     SELECT,
-    // ABILITY_1,
-    // ABILITY_2,
-    // ...
-
-    SELECT_1,
-    SELECT_2,
-    SELECT_3,
-
-
 
     // System
-    // PAUSE,
     PAUSE;
-    // FULLSCREEN,
 
     /**
      * Use to keep track of movement state
@@ -33,21 +22,4 @@ public enum Command {
     public boolean isMovement() {
         return this == MOVE_UP || this == MOVE_DOWN || this == MOVE_LEFT || this == MOVE_RIGHT;
     }
-
-    /**
-     * @return true if it is an action command
-     */
-    public boolean isAction() {
-        return this == ATTACK; // ABILITY_1 ...
-
-    }
-
-    /**
-     * @return true if it is an select command
-     */
-    public boolean isSelect() {
-        return this == SELECT_1 || this == SELECT_2 || this == SELECT_3;
-    }
-
-    // isSystem() ...
 }
